eba7d59aeac3a3c5166d8cbb679fd4ae5bc674e3
rian000614 <iyanirawan71@gmail.com>
2022-09-25 22:31:30 +0700

    finishing layouting

--- app/src/main/java/com/example/appsuitrian_chap4/MainActivity.kt
+++ app/src/main/java/com/example/appsuitrian_chap4/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.appsuitrian_chap4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun rock(view: View) {}
+    fun paper(view: View) {}
+    fun scissors(view: View) {}
 }
