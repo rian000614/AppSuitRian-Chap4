3ecf098e626d93adcc761045923ef6320b3cc545
rian000614 <iyanirawan71@gmail.com>
2022-09-28 02:01:13 +0700

    logic and change icon apk

--- app/src/main/java/com/example/appsuitrian_chap4/MainActivity.kt
+++ app/src/main/java/com/example/appsuitrian_chap4/MainActivity.kt
@@ -3,84 +3,55 @@ package com.example.appsuitrian_chap4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var computerPic: ImageView
-    private lateinit var playerPic: ImageView
-    private val choices = mutableListOf<String>("Rock","Paper","Scissors")
-    lateinit var playerScoreText: TextView
-    lateinit var computerScoreText: TextView
+    lateinit var rockImageView: ImageView
+    lateinit var scissorsImageView: ImageView
+    lateinit var paperImageView: ImageView
+    lateinit var resultImageView: ImageView
+    lateinit var refreshImageView: ImageView
+
+    private fun initComponents(){
+        rockImageView = findViewById(R.id.img_rock)
+        scissorsImageView = findViewById(R.id.img_scissors)
+        paperImageView = findViewById(R.id.img_paper)
+        resultImageView = findViewById(R.id.img_result)
+        refreshImageView = findViewById(R.id.img_refresh)
+    }
+
+    private fun initListeners(){
+        rockImageView.setOnClickListener{ startGame("ROCK")}
+        scissorsImageView.setOnClickListener{ startGame("SCISSORS")}
+        paperImageView.setOnClickListener{ startGame("PAPER")}
+
+        refreshImageView.setOnClickListener {
+            rockImageView.setOnClickListener{ startGame("ROCK")}
+            scissorsImageView.setOnClickListener{ startGame("SCISSORS")}
+            paperImageView.setOnClickListener{ startGame("PAPER")}
+            resultImageView.setImageResource(R.drawable.ic_vs)
+        }
+    }
+
+    private fun startGame(option: String){
+        val computerOption = GameSuit.picRandomOption()
+        when{
+            GameSuit.isDraw(option, computerOption)-> resultImageView.setImageResource(R.drawable.ic_draw)
+            GameSuit.isWin(option, computerOption)-> resultImageView.setImageResource(R.drawable.ic_p1win)
+            else -> resultImageView.setImageResource((R.drawable.ic_p2win))
+        }
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        img_refresh.setOnClickListener {
-            clear()
-        }
-        playerScoreText = findViewById(R.id.player_score)
-        computerScoreText = findViewById(R.id.computer_score)
-    }
 
-    fun rock(view: View) {
-        when(choices.random()){
-            "Rock" ->{
-                tv_winner.text="Seri BATU dengan BATU"
-            }
-            "Paper" ->{
-
-                tv_winner.text="Player1 Kalah BATU lawan KERTAS"
-                val computerScore: Int = computerScoreText.text.toString().toInt() +1
-                computerScoreText.text = computerScore.toString()
-            }
-            "Scissors" ->{
-                tv_winner.text="Yuhu Menang BATU lawan GUNTING"
-                val playerScore: Int = playerScoreText.text.toString().toInt() +1
-                playerScoreText.text = playerScore.toString()
-            }
-        }
-    }
-    fun paper(view: View) {
-        when(choices.random()){
-            "Rock" ->{
-                tv_winner.text="Player 1 Menang KERTAS lawan BATU"
-            }
-            "Paper" ->{
-                tv_winner.text="Seri KERTAS lawan KERTAS"
-                val computerScore: Int = computerScoreText.text.toString().toInt() +1
-                computerScoreText.text = computerScore.toString()
-            }
-            "Scissors" ->{
-                tv_winner.text="Aduh player1 kalah KERTAS lawan GUNTING"
-                val playerScore: Int = playerScoreText.text.toString().toInt() +1
-                playerScoreText.text = playerScore.toString()
-            }
-        }
-    }
-    fun scissors(view: View) {
-        when(choices.random()){
-            "Rock" ->{
-                tv_winner.text="Aduh  player1 Kalah GUNTING lawan batu"
-            }
-            "Paper" ->{
-                tv_winner.text="Player 1 Menang GUNTING lawan KERTAS"
-                val computerScore: Int = computerScoreText.text.toString().toInt() +1
-                computerScoreText.text = computerScore.toString()
-            }
-            "Scissors" ->{
-                tv_winner.text="Seri GUNTING lawan GUNTING"
-                val playerScore: Int = playerScoreText.text.toString().toInt() +1
-                playerScoreText.text = playerScore.toString()
-            }
-        }
-    }
-
-    private fun clear() {
-        computerScoreText.text ="0"
-        playerScoreText.text="0"
-        tv_winner.text = "VS"
-
+        initComponents()
+        initListeners()
     }
 }
